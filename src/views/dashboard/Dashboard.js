9af7aa2af43da7e092fbf29b9549530fe48aa238
suyeon9456 <suyeon9456@naver.com>
2025-05-18 20:29:51 +0900

    refac: 대시보드 컴포넌트에서 불필요한 주석 제거 및 코드 정리

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -190,23 +190,7 @@ const Dashboard = () => {
               </h4>
               <div className="small text-body-secondary">January - July 2023</div>
             </CCol>
-            <CCol sm={7} className="d-none d-md-block">
-              {/* <CButton color="primary" className="float-end">
-                <CIcon icon={cilCloudDownload} />
-              </CButton> */}
-              {/* <CButtonGroup className="float-end me-3">
-                {[]?.map((value) => (
-                  <CButton
-                    color="outline-secondary"
-                    key={value}
-                    className="mx-0"
-                    active={value === 'Month'}
-                  >
-                    {value}
-                  </CButton>
-                ))}
-              </CButtonGroup> */}
-            </CCol>
+            <CCol sm={7} className="d-none d-md-block"></CCol>
           </CRow>
           <MainChart
             labels={minuteTimestamps}
@@ -214,30 +198,6 @@ const Dashboard = () => {
             telemetryKeys={telemetryKeys}
           />
         </CCardBody>
-        {/* <CCardFooter>
-          <CRow
-            xs={{ cols: 1, gutter: 4 }}
-            sm={{ cols: 2 }}
-            lg={{ cols: 4 }}
-            xl={{ cols: 5 }}
-            className="mb-2 text-center"
-          >
-            {progressExample.map((item, index, items) => (
-              <CCol
-                className={classNames({
-                  'd-none d-xl-block': index + 1 === items.length,
-                })}
-                key={index}
-              >
-                <div className="text-body-secondary">{item.title}</div>
-                <div className="fw-semibold text-truncate">
-                  {item.value} ({item.percent}%)
-                </div>
-                <CProgress thin className="mt-2" color={item.color} value={item.percent} />
-              </CCol>
-            ))}
-          </CRow>
-        </CCardFooter> */}
       </CCard>
       <WidgetsBrand className="mb-4" withCharts />
       <CRow>
